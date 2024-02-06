56bf163dd068e82d2515ef5b992ca19fd89dde10
Hachounet <hachounet.github@gmx.fr>
2024-02-06 16:43:27 +0100

    V0.2
    Working on Logic module

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-// to modify value in array use gameBoard.grid[val][val] = value;
 
+// gameBoard module
 const gameBoard = (function () {
 
     let grid3 = [
@@ -21,7 +21,9 @@ const gameBoard = (function () {
     };
 
     function updateCell(positionX, positionY, marqueur){
+        const isWin = logicModule.isWin()
         gameBoardObject.grid[positionX][positionY] = marqueur;
+        return isWin;
     }
 
     return {
@@ -31,4 +33,88 @@ const gameBoard = (function () {
         gameBoardObject,
     }
     
+})();
+
+//Logic module
+
+const logicModule = (function () {
+
+    const gameBoardObject = gameBoard.gameBoardObject
+
+    function play(positionX, positionY, marqueur) {
+        // Input need to be in xxx format like 111 / 012 / 222 etc.
+        let valuePlay = prompt("Where do you want to play ? row, column, number")
+        return isValid(valuePlay)
+    }
+
+    function isValid(valuePlay){
+        valuePlay = Array.from(valuePlay)
+        let positionX = valuePlay[0]
+        let positionY = valuePlay[1]
+        let marqueur = valuePlay[2]
+        // should return [1, 1, 1]
+        if (gameBoardObject.grid[positionX][positionY] === 0 ){
+            return gameBoard.updateCell(positionX, positionY, marqueur)
+        }
+        else {
+            console.log("Error, you can't play here.")
+            logicModule.play()
+        }
+    }
+
+    const isWin = () => {
+
+        // Rows
+        for (let i = 0; i < 3; i++) {
+            if (gameBoardObject.grid[i][0] !== 0 &&
+                gameBoardObject.grid[i][0] === gameBoardObject.grid[i][1] &&
+                gameBoardObject.grid[i][1] === gameBoardObject.grid[i][2]) {
+                console.log("It's a win.");
+                return true;
+            }
+        }
+    
+        // Columns
+        for (let j = 0; j < 3; j++) {
+            if (gameBoardObject.grid[0][j] !== 0 &&
+                gameBoardObject.grid[0][j] === gameBoardObject.grid[1][j] &&
+                gameBoardObject.grid[1][j] === gameBoardObject.grid[2][j]) {
+                console.log("It's a win.");
+                return true;
+            }
+        }
+    
+        // Diagonals
+        if (gameBoardObject.grid[0][0] !== 0 &&
+            gameBoardObject.grid[0][0] === gameBoardObject.grid[1][1] &&
+            gameBoardObject.grid[1][1] === gameBoardObject.grid[2][2]) {
+            console.log("It's a win.");
+            return true;
+        }
+    
+        if (gameBoardObject.grid[0][2] !== 0 &&
+            gameBoardObject.grid[0][2] === gameBoardObject.grid[1][1] &&
+            gameBoardObject.grid[1][1] === gameBoardObject.grid[2][0]) {
+            console.log("It's a win.");
+            return true;
+        }
+    
+        else {
+            console.log("No win yet.");
+        return false; }
+    }
+
+    const test = () => {
+        const board = gameBoard.getBoard()
+        console.log("Board:", board)
+        console.log("Did you got your getBoard function ?")
+    }
+
+    return {
+        test,
+        isValid,
+        play,
+        isWin,
+    }
+
 })();
