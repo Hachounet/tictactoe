eebda61b7a836853dc930eb8388bc5f4cbf2b15b
Hachounet <hachounet.github@gmx.fr>
2024-02-06 14:24:58 +0100

    V0.1
    Module gameBoard

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let gameBoard = [];
 
-function createUser(name) {
-    return {
-        name,
-        score: 0,
-        speaking() {
-            return `${this.name} have a score of ${this.score}`;
-        },
+// to modify value in array use gameBoard.grid[val][val] = value;
+
+const gameBoard = (function () {
+
+    let grid3 = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
+
+    let gameBoardObject = {grid: grid3};
+
+    const getBoard = () => {
+        return {grid: grid3}
+    }
+
+    const resetBoard = () => {
+        grid3.forEach(row => row.fill(0));
 
     };
-}
 
-const josh = createUser("Josh");
-console.log(josh.speaking())
+    function updateCell(positionX, positionY, marqueur){
+        gameBoardObject.grid[positionX][positionY] = marqueur;
+    }
+
+    return {
+        getBoard,
+        resetBoard,     
+        updateCell,
+        gameBoardObject,
+    }
+    
+})();
