2e9c30543f888ff097ff6f6f30d04060192afba0
Hachounet <hachounet.github@gmx.fr>
2024-02-07 15:58:02 +0100

    V0.3
    2 players OK
    Need to work on isTie function

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ const gameBoard = (function () {
     };
 
     function updateCell(positionX, positionY, marqueur){
-        const isWin = logicModule.isWin()
         gameBoardObject.grid[positionX][positionY] = marqueur;
+        const isWin = logicModule.isWin()
         return isWin;
     }
 
@@ -41,36 +41,78 @@ const logicModule = (function () {
 
     const gameBoardObject = gameBoard.gameBoardObject
 
-    function play(positionX, positionY, marqueur) {
-        // Input need to be in xxx format like 111 / 012 / 222 etc.
-        let valuePlay = prompt("Where do you want to play ? row, column, number")
-        return isValid(valuePlay)
+    function play(positionX, positionY) {
+        const userTurn = userLogic.getUserTurn()
+        // Input need to be in xx format like 11 / 01 / 22 etc.
+        console.log(`It's ${userTurn.name} turn.`)
+        let valuePlay = prompt("Where do you want to play ? row, column")
+        isValid(valuePlay)
     }
+/*
+    function aiPlay() {
+        // Can it win ? 
+        console.log("It's AI turn.")
+        const userMarqueur = userLogic.getUserTurn()
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (gameBoardObject.grid[i][j] === 0) {
+                    if (isWin()) {
+                        gameBoard.updateCell(i, j, userMarqueur.marqueur);
+                        const annWinner = logicModule.annWinner()
+                        return;
+                    } else {
+                        // Undo the move if it doesn't lead to a win
+                        playRandomMove()
+                        gameBoard.updateCell(i, j, 0);
+                    }
+                }
+            }
+        }
+    }
+    */
+
+
 
     function isValid(valuePlay){
+        const userMarqueur = userLogic.getUserTurn()
+        
         valuePlay = Array.from(valuePlay)
         let positionX = valuePlay[0]
         let positionY = valuePlay[1]
-        let marqueur = valuePlay[2]
-        // should return [1, 1, 1]
+        let marqueur = userMarqueur.marqueur
+        positionX = parseInt(positionX)
+        positionY = parseInt(positionY)
+        marqueur = parseInt(marqueur)
         if (gameBoardObject.grid[positionX][positionY] === 0 ){
-            return gameBoard.updateCell(positionX, positionY, marqueur)
+            gameBoard.updateCell(positionX, positionY, marqueur)
         }
         else {
             console.log("Error, you can't play here.")
-            logicModule.play()
+            play()
         }
     }
 
-    const isWin = () => {
+    const isTie = () => {
+        for (let i =0; i < 3; i++){
+            for ( let j=0; i < 3; j++){
+                if(gameBoardObject.grid[i][j] === 0 || logicModule.isWin()){
+                    return false;
+                }
+            }
+        }
+        console.log("It's a tie.")
+        return true;
+    }
 
+    const isWin = () => {
+        console.log("Current Board:", gameBoardObject);
+        const switchPlayer = userLogic.switchPlayer
         // Rows
         for (let i = 0; i < 3; i++) {
             if (gameBoardObject.grid[i][0] !== 0 &&
                 gameBoardObject.grid[i][0] === gameBoardObject.grid[i][1] &&
                 gameBoardObject.grid[i][1] === gameBoardObject.grid[i][2]) {
-                console.log("It's a win.");
-                return true;
+                return annWinner();
             }
         }
     
@@ -79,8 +121,7 @@ const logicModule = (function () {
             if (gameBoardObject.grid[0][j] !== 0 &&
                 gameBoardObject.grid[0][j] === gameBoardObject.grid[1][j] &&
                 gameBoardObject.grid[1][j] === gameBoardObject.grid[2][j]) {
-                console.log("It's a win.");
-                return true;
+                return annWinner();
             }
         }
     
@@ -88,33 +129,104 @@ const logicModule = (function () {
         if (gameBoardObject.grid[0][0] !== 0 &&
             gameBoardObject.grid[0][0] === gameBoardObject.grid[1][1] &&
             gameBoardObject.grid[1][1] === gameBoardObject.grid[2][2]) {
-            console.log("It's a win.");
-            return true;
+               return annWinner();
+                
         }
     
         if (gameBoardObject.grid[0][2] !== 0 &&
             gameBoardObject.grid[0][2] === gameBoardObject.grid[1][1] &&
             gameBoardObject.grid[1][1] === gameBoardObject.grid[2][0]) {
-            console.log("It's a win.");
-            return true;
+                return annWinner();
+                
         }
     
         else {
             console.log("No win yet.");
-        return false; }
+            switchPlayer()
+            return false; }
     }
 
-    const test = () => {
-        const board = gameBoard.getBoard()
-        console.log("Board:", board)
-        console.log("Did you got your getBoard function ?")
+    const annWinner = () => {
+        const userTurn = userLogic.getUserTurn()
+        console.log(`The winner is ${userTurn.name}`);
+        return true;
+
     }
 
     return {
-        test,
         isValid,
         play,
+        // aiPlay,
         isWin,
+        isTie,
+        annWinner,
     }
 
 })();
+
+// User factory
+
+const userLogic = (function () {
+
+let userTurn = null;
+
+
+const getUserTurn = () => {
+    return userTurn;
+}
+
+const initUserTurn = () => {
+    return userTurn = user1;
+}
+
+function userFactory(name, typeOf, marqueur){
+    return {
+        name: name,
+        typeOf: typeOf,
+        marqueur: marqueur,
+        points : 0,
+        talk() {
+            return console.log(`Hello i'm ${name}`) }
+    }
+}
+
+function playerFactory(name) {
+    return userFactory(name, "Player", "1")
+}
+
+function aiFactory(name){
+    return userFactory(name, "AI", "2")
+}
+
+const switchPlayer = () => {
+    userTurn = (userTurn === user1) ? userBot : user1;
+     if (userTurn === userBot) {
+        console.log(`Switched to ${userTurn.typeOf} turn.`)
+        const aiPlay = logicModule.play()
+    }
+    else {
+        console.log(`Switched to ${userTurn.typeOf} turn.`)
+        const play = logicModule.play()
+    }
+    
+   
+
+}
+
+
+
+return {
+    userFactory,
+    playerFactory,
+    aiFactory,
+    switchPlayer,
+    getUserTurn,
+    initUserTurn,
+}
+
+})();
+
+
+const user1 = userLogic.playerFactory("val")
+const userBot = userLogic.aiFactory("Bot")
+let initialization = userLogic.initUserTurn()
